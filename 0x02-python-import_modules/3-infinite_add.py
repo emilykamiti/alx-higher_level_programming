54aec6238f5b4deb01c3a2e7bbd2b351acbdaa28
emilykamiti <emilykamiti@gmail.com>
2022-09-22 02:10:23 -0700

    Modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+able file 8 lines (8 sloc) 208 Bytes
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    total = 0
+    argc = len(argv) - 1
+    for i in range(1, argc + 1):
+        total = total + int(argv[i])
+    print("{:d}".format(total))
