5b89bf8b17285ce820463b443e132a4d898c159c
emilykamiti <emilykamiti@gmail.com>
2023-01-16 20:14:29 -0800

    mit

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Task 4:
+Fetches https://intranet.hbtn.io/status
+4-hbtn_status.py
+"""
+import requests
+
+if __name__ == "__main__":
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Task 6:
+Sends POST request with an email as a parameter
+To display the body of the response
+6-post_email.py
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests.post(argv[1], data={'email': argv[2]})
+    print(req.text)
